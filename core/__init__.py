858cc8a4dd2ed57e322e2a0dfe1014e79b132ab5
RozChan <67196643+RozChan@users.noreply.github.com>
2026-07-09 10:24:59 +0800

    Improve signal review defaults and card visibility

--- core/__init__.py
+++ core/__init__.py
@@ -2,5 +2,6 @@
 
 from .ai_review import run_ai_review
 from .pipeline import run_all, run_compare, run_dedup, run_extract
+from .final_export import export_final_review_result
 
-__all__ = ["run_all", "run_extract", "run_dedup", "run_compare", "run_ai_review"]
+__all__ = ["run_all", "run_extract", "run_dedup", "run_compare", "run_ai_review", "export_final_review_result"]
